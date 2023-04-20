d55a6c8b7b0815fe0a26c20b13dd4b69dbef2782
JakubMaczka00 <xerad123@gmail.com>
2023-04-20 12:51:00 +0200

    Added getting by id method

--- src/main/java/com/example/book_review_system/service/BookService.java
+++ src/main/java/com/example/book_review_system/service/BookService.java
@@ -1,6 +1,7 @@
 package com.example.book_review_system.service;
 
 import com.example.book_review_system.dto.BookResDTO;
+import com.example.book_review_system.entity.Book;
 import com.example.book_review_system.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,5 +22,14 @@ public class BookService {
                                         .stream()
                                         .map(BookResDTO::new)
                                         .collect(Collectors.toList());
+    }
+    public BookResDTO getBook(Long bookId){
+        Book book = bookRepository.getOne(bookId);
+        return BookResDTO.builder()
+                .bookId(book.getId())
+                .author(book.getAuthor())
+                .title(book.getTitle())
+                .isActive(book.getIsActive())
+                .build();
     }
 }
