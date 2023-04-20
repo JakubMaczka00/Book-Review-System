0b165e833a586d6c263a4a5583de1cc990396bc6
JakubMaczka00 <xerad123@gmail.com>
2023-04-20 12:51:27 +0200

    Added new endpoint

--- src/main/java/com/example/book_review_system/controller/BookController.java
+++ src/main/java/com/example/book_review_system/controller/BookController.java
@@ -1,8 +1,10 @@
 package com.example.book_review_system.controller;
 
+import com.example.book_review_system.dto.BookResDTO;
 import com.example.book_review_system.service.BookService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +16,12 @@ public class BookController {
     public BookController(BookService bookService) {
         this.bookService = bookService;
     }
-    @GetMapping("/book/all")
+
+    @GetMapping("/books/all")
     public ResponseEntity<?> getAllBooks() { return ResponseEntity.ok(bookService.getAllBooks());}
+
+    @GetMapping("/books/{bookId}")
+    public ResponseEntity<BookResDTO> getBook(@PathVariable("bookId") Long bookId){
+        return ResponseEntity.ok(bookService.getBook(bookId));
+    }
 }
