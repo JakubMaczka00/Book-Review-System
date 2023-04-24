5348e3f4be019ff33dae72752d32ade851322508
JakubMaczka00 <xerad123@gmail.com>
2023-04-24 12:33:13 +0200

    Added HttpHeaders class

---
+++ src/main/java/com/example/book_review_system/component/HeaderHelper.java
@@ -0,0 +1,10 @@
+package com.example.book_review_system.component;
+import org.springframework.http.HttpHeaders;
+public class HeaderHelper {
+    private final static String ADDITIONAL_MESSAGE = "messageSuccess";
+    public static HttpHeaders getHeadersMessage(String message){
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set(ADDITIONAL_MESSAGE, message);
+        return responseHeaders;
+    }
+}
