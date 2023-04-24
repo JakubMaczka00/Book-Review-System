3166a4c688df69c74af56eb5c8e4bf5e52dc04c6
JakubMaczka00 <xerad123@gmail.com>
2023-04-24 12:33:35 +0200

    Added Messages

---
+++ src/main/java/com/example/book_review_system/constant/Message.java
@@ -0,0 +1,9 @@
+package com.example.book_review_system.constant;
+
+public class Message {
+    //BOOK
+    public static final String BOOK_CREATED = "FEEDBACK.BOOK_CREATED";
+    public static final String BOOK_EXISTS = "FEEDBACK.BOOK_EXISTS";
+    public static final String BOOK_CANNOT_BE_DELETED = "FEEDBACK.BOOK_CANNOT_BE_DELETED";
+    public static final String BOOK_DELETED = "FEEDBACK.BOOK_DELETED";
+}
