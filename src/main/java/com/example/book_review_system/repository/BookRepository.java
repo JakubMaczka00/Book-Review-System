6e1a5e4155a7a46159febb9a55c831971c2edf0a
JakubMaczka00 <xerad123@gmail.com>
2023-04-24 12:32:08 +0200

    Added new query

--- src/main/java/com/example/book_review_system/repository/BookRepository.java
+++ src/main/java/com/example/book_review_system/repository/BookRepository.java
@@ -3,6 +3,7 @@ package com.example.book_review_system.repository;
 import com.example.book_review_system.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -12,5 +13,10 @@ public interface BookRepository extends JpaRepository<Book, Long> {
             "FROM Book b " +
             "WHERE  b.isActive = TRUE ")
     List<Book> getAllBooks();
+
+    @Query("SELECT CASE WHEN COUNT(b) > 0 THEN TRUE ELSE FALSE END " +
+            "FROM Book b " +
+            "WHERE b.title = :title AND b.isActive = TRUE ")
+    boolean existByTitle(@Param("title") String title);
 
 }
