f33d599d1e7ccf075dbe6867d654fb22f91d9938
JakubMaczka00 <xerad123@gmail.com>
2023-04-27 07:47:19 +0200

    Added new repository

---
+++ src/main/java/com/example/book_review_system/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.book_review_system.repository;
+
+import com.example.book_review_system.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+}
