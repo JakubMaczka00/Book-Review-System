e2f4971368e8366e04302336780fa39b27e86656
JakubMaczka00 <xerad123@gmail.com>
2023-04-27 07:46:41 +0200

    Added new DTOS

---
+++ src/main/java/com/example/book_review_system/dto/request/LoginReqDTO.java
@@ -0,0 +1,15 @@
+package com.example.book_review_system.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginReqDTO {
+    private String email;
+     String password;
+}
---
+++ src/main/java/com/example/book_review_system/dto/response/AuthResDTO.java
@@ -0,0 +1,14 @@
+package com.example.book_review_system.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResDTO {
+    private String token;
+}
